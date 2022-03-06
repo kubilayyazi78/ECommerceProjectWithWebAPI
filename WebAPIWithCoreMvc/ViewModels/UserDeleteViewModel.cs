b4382a914be575b1e60da54dfd5ab9245ddbaf15
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-03-06 16:15:12 +0300

    Core Mvc WebAPI ile silme işlemi gerçekleştirildi.

---
+++ WebAPIWithCoreMvc/ViewModels/UserDeleteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIWithCoreMvc.ViewModels
+{
+    public class UserDeleteViewModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string GenderName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
