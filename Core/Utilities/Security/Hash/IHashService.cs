3e9f6589bfd4a359d2ba2be6c8f9a3f272f8f453
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-04-24 02:49:35 +0300

    Hash (Sha256) ve MD5 Şifreleme işlemleri gerçekleştirildi.

---
+++ Core/Utilities/Security/Hash/IHashService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hash
+{
+    public interface IHashService
+    {
+        string CreateHash(string plainText);
+        bool CompareHash(string hashedText,string plainText );
+
+    }
+}
---
+++ Core/Utilities/Security/Hash/Sha256/Sha256HashService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hash.Sha256
+{
+    public class Sha256HashService : IHashService
+    {
+        public string CreateHash(string plainText)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public bool CompareHash(string hashedText, string plainText)
+        {
+            return CreateHash(plainText) == hashedText;
+        }
+    }
+}
