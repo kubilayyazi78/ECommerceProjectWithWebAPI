9b58a99a24871cd58260090117bf076702c25810
kubilayyazi78 <kubilayyazi@hotmail.com>
2023-01-31 23:33:20 +0300

    ResourceDetail business katmanında geliştirmeler yapıldı.

---
+++ Entities/Dtos/ResourceDetails/ResourceDetailAddDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using System;
+
+namespace Entities.Dtos.ResourceDetails
+{
+    public class ResourceDetailAddDto : IDto
+    {
+        public string ResourceValue { get; set; }
+        public int LanguageID { get; set; }
+        public int ResourceID { get; set; }
+    }
+}
