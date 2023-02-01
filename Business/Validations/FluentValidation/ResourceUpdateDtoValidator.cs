d345e4963eb022743a950f323f6dac79f72760ec
kubilayyazi78 <kubilayyazi@hotmail.com>
2023-02-01 23:39:52 +0300

    Bug fixed

--- Business/Validations/FluentValidation/ResourceUpdateDtoValidator.cs
+++ Business/Validations/FluentValidation/ResourceUpdateDtoValidator.cs
@@ -9,15 +9,6 @@ namespace Business.Validations.FluentValidation
         {
             RuleFor(x => x.ResourceName).NotEmpty().
                 WithErrorCode("VALIDATION_ResourceNameFieldCannotBeEmpty");
-
-            RuleFor(x => x.ResourceValue).NotEmpty().
-                WithErrorCode("VALIDATION_ResourceValueFieldCannotBeEmpty");
-
-            RuleFor(x => x.ResourceGroupName).NotEmpty().
-               WithErrorCode("VALIDATION_ResourceGroupNameFieldCannotBeEmpty");
-
-            RuleFor(x => x.LanguageID).NotEmpty().
-         WithErrorCode("VALIDATION_LanguageNameFieldCannotBeEmpty");
         }
     }
 }
